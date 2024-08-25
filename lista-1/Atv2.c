e2c5e0f0071b040d1cb58d9d00116e9ee6d2eba7
Láisa Liane <laisaliane22@gmail.com>
2024-08-25 17:19:07 -0300

    atv2

---
+++ lista-1/Atv2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    int n1, n2, soma;
+
+    printf("Informe o primeiro numero: ");
+    scanf("%d", &n1);
+    printf("Informe o segundo numero: " );
+    scanf("%d", &n2);
+
+    soma = n1 + n2;
+    printf("A soma desses dois numeros e igual a: ", soma);
+
+}
