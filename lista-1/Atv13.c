c66f73c4c3e4ff9133dc90aa610f145ad21812f5
Láisa Liane <laisaliane22@gmail.com>
2024-08-26 20:51:24 -0300

    atv14

---
+++ lista-1/Atv13.c
@@ -0,0 +1,12 @@
+Desenvolva um programa para calcular a distância entre dois pontos em um plano 2D.
+Use as fórmulas: distância = sqrt((x2 - x1)^2 + (y2 - y1)^2) (utilize math.h para a função
+sqrt()).
+
+#include <stdio.h>
+#include <math.h>
+int main () {
+    int x, y;
+
+    sqrt((x2 - x1)^2 + (y2 - y1)^2);
+
+}
---
+++ lista-1/Atv14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () {
+    int a,b;
+    printf("Informe o valor: \n");
+    scanf("%d", &a);
+    printf("Informe outro valor: \n");
+    scanf("%d", &b);
+
+    printf("os valores informados sao: %d %d \n",a,b);
+    printf("os valores informados sao: %d %d",b,a);
+
+}
