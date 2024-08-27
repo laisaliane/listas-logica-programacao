1068d81a992190eacefd29dda6dd2d0f21b0b5c9
Láisa Liane <laisaliane22@gmail.com>
2024-08-26 21:52:16 -0300

    atv16

---
+++ lista-1/Atv16.c
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdio.h>
+int main () {
+    int c,v;
+    printf("Informe o valor: \n");
+    scanf("%d", &c);
+    v = c*c*c;
+
+    printf("O volume do triangulo e: %d", v);
+}
