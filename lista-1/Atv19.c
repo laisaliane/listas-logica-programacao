c3d319f3d09457b6f8d6789f28cb478acd10aaf7
Láisa Liane <laisaliane22@gmail.com>
2024-08-26 22:29:14 -0300

    atv19

---
+++ lista-1/Atv19.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+    int m,c;
+    printf("Informe a medida em metros: \n");
+    scanf("%d", &m);
+    c = m * 100;
+    printf("O valor em centimetros equivale a: %d", c);
+
+}
