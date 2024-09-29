8c994d7506c32324dafe576b6628f91bb77337cd
Láisa Liane <laisaliane22@gmail.com>
2024-09-29 02:45:26 -0300

    questao6

--- lista-4/At6.4.c
+++ lista-4/At6.4.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 
 int main () {
+
+    int numA, numB;
+    char operador;
+
     printf("Calculadora Simples \n");
     printf("++++++++++++++++++++++++++++++++++ \n");
 
+    printf("Qual o operador deseja: \n");
+    operador = getchar();
 
+    printf("Informe o primeiro numero: \n");
+    scanf("%d", &numA);
+
+    printf("Informe o segundo numero: \n");
+    scanf("%d", &numB);
+
+    if (operador == '+') {
+        printf("o resultado e %d ", numA + numB);
+    }
+
+    if (operador == '-') {
+        printf("o resultado e %d ", numA - numB);
+    }
+
+    if (operador == '*') {
+        printf("o resultado e %d ", numA * numB);
+    }
+
+    if (operador == '/') {
+        printf("o resultado e %d ", numA / numB);
+    }
 }
--- lista-4/At8.4.c
+++ lista-4/At8.4.c
@@ -1,3 +1,16 @@
-//
-// Created by Lucio Santana on 28/09/2024.
-//
+#include <stdio.h>
+
+int main () {
+
+    float celcius, far, kelv;
+    char temperatura [50] = "";
+
+    printf("Conversao de Temperatura \n");
+    printf("++++++++++++++++++++++++++ \n");
+
+    printf("Informe a temperadura atual em C \n");
+    scanf("%f", &celcius);
+
+    printf("Deseja transformar em Fahrenheit ou Kelvin? \n");
+    scanf("%s", &temperatura);
+}
