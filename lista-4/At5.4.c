0ee9a2ec258c50c5a9f57fe8d72051f830ebb083
Láisa Liane <laisaliane22@gmail.com>
2024-09-29 00:43:41 -0300

    questao5

--- lista-4/At5.4.c
+++ lista-4/At5.4.c
@@ -1,3 +1,28 @@
-//
-// Created by Lucio Santana on 28/09/2024.
-//
+#include <stdio.h>
+
+int main () {
+
+    float nota;
+
+    printf("Classificacao de Nota \n");
+    printf("++++++++++++++++++++++++++++++ \n");
+
+    printf("Informe a nota do aluno (a) \n");
+    scanf("%f", &nota);
+
+    if (nota <= 4) {
+        printf("Nota insuficiente");
+    }
+
+    if (nota >= 5 && nota <= 6) {
+        printf("Nota regular");
+    }
+
+    if (nota  >= 7 && nota <= 8) {
+        printf("Bom");
+    }
+
+    else {
+        printf("Otimo");
+    }
+}
