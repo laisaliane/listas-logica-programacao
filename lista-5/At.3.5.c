738a868ecad419b8d7bb734f31d23cc793549210
Láisa Liane <laisaliane22@gmail.com>
2024-10-06 19:57:50 -0300

    questao3

--- lista-5/At.3.5.c
+++ lista-5/At.3.5.c
@@ -1,3 +1,23 @@
-//
-// Created by Lucio Santana on 05/10/2024.
-//
+#include <stdio.h>
+
+// As maçãs custam R$ 0,30 cada se forem compradas menos que uma dúzia.
+// E R$ 0,25 se forem compradas pelo menos doze. Escreva um programa que leia
+// o número de maçãs compradas, calcule e escreva o valor total da compra.
+
+int main () {
+    printf("Compras de macas \n");
+    printf("--------------------------- \n");
+
+    float quantidade, valor;
+
+    printf("Informe a quantidade de macas adquiridas: \n");
+    scanf("%f", &quantidade);
+
+    if (quantidade < 12) {
+        printf("O valor total da compra eh %2.f", quantidade * 0.30);
+    }
+
+    if (quantidade >= 12) {
+        printf("O valor total da compra eh %2.f", quantidade * 0.25);
+    }
+}
